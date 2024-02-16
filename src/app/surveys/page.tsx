a1cda387337c8adf6236744750348a2930f88758
na <nadk@aladdinsys.co.kr>
2024-02-16 09:55:13 +0900

    Hotfix 다이나믹 아닌 page 추가

---
+++ src/app/surveys/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {get} from "@/services/fetch";
+import {Response} from "@/services/types/response";
+import {SurveyResponse} from "@/services/types/survey-response";
+import SurveyComponent from "@/components/Survey/SurveyComponent";
+
+const Survey = async () => {
+
+    const response: Response<SurveyResponse> = await get<Response<SurveyResponse>>(`/open-api/survey/ba68cd2c-ce90-4174-9691-b746c24cfae0`);
+    const surveyResponse: SurveyResponse = response.result;
+
+    return (
+        <SurveyComponent survey={surveyResponse}/>
+    );
+}
+export default Survey;
